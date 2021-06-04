5176fd66e0cd1b9c9da3e955172d99b1a60c7bc1
fullsnack-DEV <pratikpatre2019@gmail.com>
2021-06-04 09:54:45 +0530

    Added fonts

---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const HomePage = () => {
+  return <></>;
+};
+
+//TODO
+//To build a Custome Hook that will fetch the data
+//Make aUSe of Redux To implement Add to Cart Functionality
