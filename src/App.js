37c097d40c94441fad68002536b4e542eab1ef4b
fullsnack-DEV <pratikpatre2019@gmail.com>
2021-06-19 11:33:21 +0530

    Added te dispatcher for "remind Later"

--- src/App.js
+++ src/App.js
@@ -3,14 +3,20 @@ import "./App.scss";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { HomePage } from "./Pages/HomePage";
 import { DetailPage } from "./Pages/DetailPage";
+import { Provider } from "react-redux";
+import Store from "./Redux/store.jsx";
+import { FavPage } from "./Pages/FavPage";
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/detail/:id" component={DetailPage} />
-      </Switch>
-    </Router>
+    <Provider store={Store}>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/detail/:id" component={DetailPage} />
+          <Route exact path="/fav" component={FavPage} />
+        </Switch>
+      </Router>
+    </Provider>
   );
 }
 
