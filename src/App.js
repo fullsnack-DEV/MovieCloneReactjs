79ddec5cf45941d18ad15d995c152683b34e6865
fullsnack-DEV <pratikpatre2019@gmail.com>
2021-06-27 22:29:55 +0530

    minor Changes

--- src/App.js
+++ src/App.js
@@ -7,38 +7,36 @@ import { Provider } from "react-redux";
 import Store from "./Redux/store.jsx";
 import { FavPage } from "./Pages/FavPage";
 import { Mobtabs } from "./component/Mobtabs";
-import { useMedia } from "./Hooks/useMedia";
+import { useMedia } from "./Hooks/UseMedia";
 import { Recommended } from "./Pages/Recommended";
 import { MobTicket } from "./Pages/MobTicket";
 function App() {
-  const phone = useMedia("(max-width:500px )");
+  const isphone = useMedia("(max-width:500px )");
 
   return (
     <>
-      {phone ? (
-        <Provider store={Store}>
-          <Router>
-            <Switch>
-              <Route exact path="/" component={HomePage} />
-              <Route exact path="/detail/:id" component={DetailPage} />
-              <Route exact path="/fav" component={FavPage} />
-              <Route exact path="/recomanded" component={Recommended} />
-              <Route exact path="/ticket" component={MobTicket} />
-            </Switch>
-            <Mobtabs />
-          </Router>
-        </Provider>
-      ) : (
-        <Provider store={Store}>
-          <Router>
-            <Switch>
-              <Route exact path="/" component={HomePage} />
-              <Route exact path="/detail/:id" component={DetailPage} />
-              <Route exact path="/fav" component={FavPage} />
-            </Switch>
-          </Router>
-        </Provider>
-      )}
+      <Provider store={Store}>
+        <Router>
+          <Switch>
+            {isphone ? (
+              <Switch>
+                <Route exact path="/" component={HomePage} />
+                <Route exact path="/detail/:id" component={DetailPage} />
+                <Route exact path="/fav" component={FavPage} />
+                <Route exact path="/recomanded" component={Recommended} />
+                <Route exact path="/ticket" component={MobTicket} />
+              </Switch>
+            ) : (
+              <Switch>
+                <Route exact path="/" component={HomePage} />
+                <Route exact path="/detail/:id" component={DetailPage} />
+                <Route exact path="/fav" component={FavPage} />
+              </Switch>
+            )}
+          </Switch>
+          <Mobtabs />
+        </Router>
+      </Provider>
     </>
   );
 }
